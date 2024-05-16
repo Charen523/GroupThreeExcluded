2bd797ad500aac837f611617090499a2852e6cb8
DoOrNo33 <167051416+DoOrNo33@users.noreply.github.com>
2024-05-16 17:51:45 +0900

    [Add] AttackSO.cs
    AttackSO 스크립트 및 적BasicAttackSO 파일 생

---
+++ Group3NotIncluded/Assets/ScriptableObjects/Scripts/AttackSO.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DefaultAttackSO", menuName = "GameController/Attacks/Default", order = 0)]
+public class AttackSO : ScriptableObject
+{
+    [Header("Attack Info")]
+    public float size;
+    public float delay;
+    public float power;
+    public float speed;
+    public LayerMask target;
+
+    [Header("Ranged Attack Data")]
+    public string bulletNameTag;
+    public float duration;
+    public float spread;
+    public int numberofProjectilesPerShot;
+    public float multipleProjectilesAngle;
+    public Color projectileColor;
+}
