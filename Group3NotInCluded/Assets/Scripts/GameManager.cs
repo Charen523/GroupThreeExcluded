ec0e9b7c531989c374713ec15c94530be5dd7a39
KIMSHINWOO <48780618+cheerio31608@users.noreply.github.com>
2024-05-16 20:10:18 +0900

    [feat]Set/Decrease playerHp count
    플레이어의 초기 체력 카운트 세팅,
    공격에 맞을 때 마다 체력 카운트 감소

--- Group3NotInCluded/Assets/Scripts/GameManager.cs
+++ Group3NotInCluded/Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -11,6 +12,8 @@ public class GameManager : MonoBehaviour
     [Header ("UI")]
     public TextMeshProUGUI timer;
     public TextMeshProUGUI gameScore;
+    public GameObject[] playerHpUI; 
+    public int playerHpCount;
 
     [Header("PanelList")]
     public GameObject menuPanel;
@@ -42,7 +45,11 @@ public class GameManager : MonoBehaviour
     {
         Time.timeScale = 1.0f;
         // 플레이어 생성 구현
+
+        //플레이어 Hp 구현
+        CreateHpUI();
     }
+
 
     // Update is called once per frame
     void Update()
@@ -54,7 +61,17 @@ public class GameManager : MonoBehaviour
         // 몬스터 생성 구현(Invoke 또는 Couroutine 사용하여 시간차 두기)
 
         // 특정 조건 달성(플레이어 체력 0)시 게임 종료
-        // EndGame();
+        //if (playerHpCount <= 0)
+        //{
+        //    EndGame();
+        //}
+    }
+
+    // 프리펩화 한 Hp UI를 playerHpCount수 만큼 생성
+    // 특정 좌표에 프리펩 생성, n만큼 x좌표 이동
+    private void CreateHpUI()
+    {
+        Vector3 position = new Vector3(10f, 0f, 0f); //좌표 수정 필요!
     }
 
     // 적이 처치될 때 마다 호출
@@ -63,6 +80,14 @@ public class GameManager : MonoBehaviour
     {
         score++; // 점수 계산 방식에 따라 조정 필요
         gameScore.text = score.ToString();
+    }
+
+    // 플레이어가 공격에 맞을 때 마다 호출
+    // 플레이어 Hp UI 1개 소멸
+    public void DecreaseHp()
+    {
+        Destroy(playerHpUI[playerHpCount - 1]);
+        playerHpCount--;
     }
 
 
