bedb3c97ff0fd6dc3a8f330b7164a2cba8704aa4
Chloe <144107013+Charen523@users.noreply.github.com>
2024-05-16 17:28:17 +0900

    [feat] 입력에 따른 스크립트 구현
    우선 P1 기준으로 WASD로 이동, R로 부스트.

---
+++ Group3NotInCluded/Assets/Scripts/Entities/Behaviours/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private PlayerInputController controller;
+    private Rigidbody2D rb;
+
+    private bool isBoost;
+    private readonly float SPEED = 5f; //캐릭터 스탯으로 이동 필요.
+    private Vector2 playerDirection = Vector2.zero;
+
+    private void Awake()
+    {
+        controller = GetComponent<PlayerInputController>();
+        rb = GetComponent<Rigidbody2D>();   
+    }
+
+    private void Start()
+    {
+        controller.OnMoveEvent += Move;
+        controller.OnBoostEvent += Boost;
+
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement(playerDirection);
+    }
+
+    private void Move(Vector2 direction)
+    {
+        playerDirection = direction;
+    }
+
+    private void Boost(bool isPressed)
+    {
+        isBoost = isPressed;    
+    }
+
+    private void ApplyMovement(Vector2 direction)
+    {
+        //boost 중이면 이속 * 3
+        rb.velocity = direction * SPEED * (isBoost ? 3 : 1);
+    }
+}
