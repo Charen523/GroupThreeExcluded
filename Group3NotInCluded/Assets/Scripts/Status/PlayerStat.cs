b6676bdef241a5250f6751180c1ada39e3eea188
Chloe <144107013+Charen523@users.noreply.github.com>
2024-05-17 15:49:04 +0900

    [refactor] 부스트&스피드 최대치 적정값으로 수정

--- Group3NotInCluded/Assets/Scripts/Status/PlayerStat.cs
+++ Group3NotInCluded/Assets/Scripts/Status/PlayerStat.cs
@@ -6,6 +6,6 @@ public class PlayerStat : UnitStat
 {
     //EnemyStat으로부터 maxHealth와 attackSO 상속받음.
     public StatsChangeType statsChangeType;
-    [Range(1f, 20f)] public float speed; //플레이어 이속
-    [Range(1f, 10f)] public float boostGage;
+    [Range(1f, 10f)] public float speed; //플레이어 이속
+    [Range(1f, 3f)] public float maxBoostGage;
 }
