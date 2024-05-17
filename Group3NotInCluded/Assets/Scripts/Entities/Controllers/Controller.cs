4031dfbae8138801d3f7230d011ae39071fade90
DoOrNo33 <167051416+DoOrNo33@users.noreply.github.com>
2024-05-17 11:44:13 +0900

    [Refactor] Delay
    공격 딜레이 추가

--- Group3NotInCluded/Assets/Scripts/Entities/Controllers/Controller.cs
+++ Group3NotInCluded/Assets/Scripts/Entities/Controllers/Controller.cs
@@ -5,8 +5,39 @@ public class Controller : MonoBehaviour
 {
     public event Action<AttackSO> OnAttackEvent;
 
+    private float timeSinceLastAttack = float.MaxValue;
+
+    protected EnemyStatHandler stats { get; private set; }
+
+    protected virtual void Awake()
+    {
+        stats = GetComponent<EnemyStatHandler>();
+    }
+
+    protected virtual void Update()
+    {
+        HandleAttackDelay();
+    }
+
+    private void HandleAttackDelay()
+    {
+        // 공격 딜레이
+        if (timeSinceLastAttack <= stats.currentStat.attackSO.delay)
+        {
+            timeSinceLastAttack += Time.deltaTime;
+        }
+        // 공격
+        if (timeSinceLastAttack > stats.currentStat.attackSO.delay)
+        {
+            timeSinceLastAttack = 0;
+            CallAttackEvent(stats.currentStat.attackSO);
+        }
+    }
+
     public void CallAttackEvent(AttackSO attackSO)
     {
         OnAttackEvent?.Invoke(attackSO);
     }
+
+
 }
