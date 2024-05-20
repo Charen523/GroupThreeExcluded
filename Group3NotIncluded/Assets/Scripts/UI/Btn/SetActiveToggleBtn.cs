fa43cab8dab60fabc76d67457b73876a84c5cb3e
Chloe <144107013+Charen523@users.noreply.github.com>
2024-05-20 09:18:13 +0900

    [chore] 토글스크립트 주석삭제

--- Group3NotIncluded/Assets/Scripts/UI/Btn/SetActiveToggleBtn.cs
+++ Group3NotIncluded/Assets/Scripts/UI/Btn/SetActiveToggleBtn.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class SetActiveToggleBtn : MonoBehaviour
 {
-    //약간 문제가 있지만 작동은 함. 5.20 튜터 찾아가기.
     public void ToggleGameObject(GameObject obj)
     {
        obj.SetActive(!obj.activeSelf);
