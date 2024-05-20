51abe3ea6a0061caf65fc60b68f654752d728e1d
Chloe <144107013+Charen523@users.noreply.github.com>
2024-05-20 17:56:30 +0900

    [feat&fix] 랜덤 아이템 출력
    feat: 아이템 랜덤으로 출력
    fix: 이벤트 리스너가 중복되는 문제 해결: Ondisable

--- Group3NotIncluded/Assets/Scripts/Entities/Controllers/BubbleController.cs
+++ Group3NotIncluded/Assets/Scripts/Entities/Controllers/BubbleController.cs
@@ -20,6 +20,11 @@ public class BubbleController : MonoBehaviour
         managers.OnEnemyDie += MakeBubble;
     }
 
+    private void OnDisable()
+    {
+        managers.OnEnemyDie -= MakeBubble;
+    }
+
     public void MakeBubble(GameObject enemy)
     {
         Vector3 instantiatePos = enemy.transform.position;
@@ -33,12 +38,16 @@ public class BubbleController : MonoBehaviour
         else
             instantiatePos.x -= 0.5f;
 
-        int randomNum = UnityEngine.Random.Range(0, 4);
-        if (true)
-        {
-            GameObject newBubble = Instantiate(itemBubble);
-            newBubble.transform.position = instantiatePos;
-        }
+        GameObject newBubble = Instantiate(itemBubble);
+
+        int randomItemNum = UnityEngine.Random.Range(0, 5);
+
+        SpriteRenderer spriteRenderer = newBubble.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = Resources.Load<Sprite>($"beffio/Icons/Icon{randomItemNum}");
+
+        newBubble.transform.position = instantiatePos;
+
+        Debug.Log("¾ß");
     }
 
     public void InvokeItem0()
