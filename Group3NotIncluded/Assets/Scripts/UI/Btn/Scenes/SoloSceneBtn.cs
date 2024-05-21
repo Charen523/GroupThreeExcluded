93a730b7730029237816f5a5f30e30b0e6ca9947
Fluency5427 <167179609+Fluency5427@users.noreply.github.com>
2024-05-21 16:04:59 +0900

    feat,add UI
    솔로씬 버튼 스크립트, 오디오매니저 스크립트 수정, 뮤직버튼 스크립트, 불륨슬라이더컨트롤 스크립트 추가,

--- Group3NotIncluded/Assets/Scripts/UI/Btn/Scenes/SoloSceneBtn.cs
+++ Group3NotIncluded/Assets/Scripts/UI/Btn/Scenes/SoloSceneBtn.cs
@@ -3,8 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class SoloSceneBtn : MonoBehaviour
 {
-    public AudioClip[] sceneBackgroundMusics;
-
     public void LoadSoloScene()
     {
         SceneManager.LoadScene(1);
@@ -12,7 +10,14 @@ public class SoloSceneBtn : MonoBehaviour
         if (Managers.Instance.enemyManager == null) return;
         Managers.Instance.enemyManager.ClearEnemyManager();
     }
-        public void PlayEffectSound()
+
+    void Start()
+    {
+        // 첫 번째 배경음악 재생
+        AudioManager.Instance.PlayBackgroundMusic(1);
+    }
+
+    public void PlayEffectSound()
     {
         //효과음 재생
         AudioManager.Instance.PlaySFX(0);
